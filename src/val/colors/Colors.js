5db4cca08f2194c6af678dc16f182e6ea2c3fe6a
SobhanBera <sobhanbera258@gmail.com>
2021-01-08 22:07:43 +0530

    added colors for light theme

--- src/val/colors/Colors.js
+++ src/val/colors/Colors.js
@@ -14,7 +14,10 @@ const COLORS = {
 	SOMEWHATDARKALPHA: 'rgba(0, 0, 0, 0.55)',
 
 	PLACEHOLDER: '#bfbfbf',
+	DARKPLACEHOLDER: '#757575',
 	TEXT: '#efefff',
+	DARKTEXT: '#252525',
+	MID: '#808080',
 
 	PINK: '#EF476F',
 	YELLOW: '#FFD166',
@@ -34,11 +37,47 @@ const COLORS = {
 	TRANSPARENT: 'transparent' || '#0000',
 };
 
-export const RANDOM_COLOR = () => {
-	let array = [];
-	for (let i in COLOR) array.push(COLOR[i]);
-	console.table(array);
-	return array[Math.floor(Math.random() * array.length)];
-};
+class DarkLight {
+	constructor() {
+		// variables...
+		this.array = [];
+
+		// methods...
+		this.RANDOM_COLOR = () => {
+			console.table(this.array);
+			return this.array[Math.floor(Math.random() * this.array.length)];
+		};
+
+		this.colorIsLight = color => {
+			let r, g, b, hsp;
+			if (color.match(/^rgb/)) {
+				color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+				r = color[1];
+				g = color[2];
+				b = color[3];
+			} else {
+				color = +('0x' + color.slice(1).replace(color.length < 5 && /./g, '$&$&'));
+				r = color >> 16;
+				g = (color >> 8) & 255;
+				b = color & 255;
+			}
+
+			hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+			//return true if the givne colro in the argument is light...
+			// console.log(hsp, hsp > 127.5);
+			return hsp < 127.5;
+		};
+		this.loadColorArray();
+	}
+
+	loadColorArray = () => {
+		let arr = [];
+		for (let i in COLORS) arr.push(COLORS[i]);
+		this.array = arr;
+		// console.log(this.array);
+	};
+}
+
+export const ISDARKCOLOR = new DarkLight();
 
 export default COLORS;
