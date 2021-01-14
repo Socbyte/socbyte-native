180bb98564dc93573fecc0e6ef08c2cc24b27e1e
SobhanBera <sobhanbera258@gmail.com>
2021-01-14 12:07:17 +0530

    rest configs

--- src/scenes/main/Home.js
+++ src/scenes/main/Home.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import {
+	IconButton,
+	Button,
+	Menu,
+	Provider,
+	Divider,
+	DarkTheme,
+	DefaultTheme,
+} from 'react-native-paper';
+import { Ionicons } from '@expo/vector-icons';
 
 import COLORS from '../../val/colors/Colors';
 
 const Home = props => {
+	const [menu, setMenu] = useState(false);
 	return (
-		<View>
-			<Text style={{ color: COLORS.WHITE }}>HOME</Text>
+		<View style={styles.screen}>
+			{/* <Provider theme={DarkTheme}>
+				<View>
+					<Menu
+						visible={menu}
+						onDismiss={() => setMenu(false)}
+						anchor={
+							<Button
+								onPress={() => {
+									setMenu(true);
+								}}>
+								Show Menu
+							</Button>
+						}>
+						<Menu.Item onPress={() => {}} title='Item 1' />
+						<Menu.Item onPress={() => {}} title='Item 2' />
+						<Divider />
+						<Menu.Item onPress={() => {}} title='Item 3' />
+					</Menu>
+				</View>
+			</Provider> */}
+
+			<Provider theme={DarkTheme}>
+				<View
+					style={{
+						flex: 1,
+						alignContent: 'center',
+						justifyContent: 'center',
+						// backgroundColor: COLORS.ACCENT,
+					}}>
+					<Menu
+						style={
+							{
+								// backgroundColor: COLORS.ACCENT,
+							}
+						}
+						visible={menu}
+						onDismiss={() => setMenu(false)}
+						anchor={
+							<IconButton
+								style={{
+									margin: 0,
+								}}
+								onPress={() => {
+									props.navigation.toggleDrawer();
+								}}
+								icon={() => (
+									<Ionicons name='menu' color={COLORS.DARKSECONDARY} size={26} />
+								)}
+							/>
+						}>
+						<Menu.Item onPress={() => {}} title='Item 1' />
+						<Menu.Item onPress={() => {}} title='Item 2' />
+						<Divider />
+						<Menu.Item onPress={() => {}} title='Item 3' />
+					</Menu>
+				</View>
+			</Provider>
 		</View>
 	);
 };
