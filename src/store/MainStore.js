f0b76498573e985e66eda7d8c0277b4c526f0662
SobhanBera <sobhanbera258@gmail.com>
2021-01-14 12:06:21 +0530

    configured... now instead of getting data from navigation and dispatch that to store this code will directly store fetched data from server whenever an update is found

--- src/store/MainStore.js
+++ src/store/MainStore.js
@@ -1,3 +1,6 @@
+import md5 from 'md5';
+import firebase from '../firebase/Firebase';
+
 /*
  *	Action constants and
  *	Action json data provider functions and
@@ -9,11 +12,58 @@ export const SET_PROFILE_IMAGE_URL = 'SET_PROFILE_IMAGE_URL';
  * function dispatchers
  */
 export function loadUserData(userData) {
-	return { type: LOAD_USER_DATA, userData: userData };
+	//here the userData argument is useless and not required just in case may required
+	//in future new features that why it is here...
+
+	// return { type: LOAD_USER_DATA, userData: userData };
+
+	return function (dispatch, getStore) {
+		const currentUser = firebase.auth().currentUser;
+		firebase
+			.database()
+			.ref('Users')
+			.child(currentUser.displayName)
+			.on('value', snapshot => {
+				const emailHash = md5(currentUser.email);
+
+				if (snapshot.val()) {
+					dispatch({
+						type: LOAD_USER_DATA,
+						userData: snapshot.val(),
+						profileImg: `https://www.gravatar.com/avatar/${emailHash}.jpg?s=200`,
+					});
+					// const val = ImageColors.getColors(snapshot.val().coverImg, {
+					// 	fallback: '#000000',
+					// 	quality: 'low',
+					// 	pixelSpacing: 5,
+					// }).then(res => {
+					// 	console.log(res.average);
+					// 	console.log(res.dominant);
+					// 	console.log(res.lightVibrant);
+					// 	console.log(res.vibrant);
+					// 	console.log(res.darkVibrant);
+					// });
+					// dispatch({
+
+					// })
+				} else {
+					alert('user data could not be loaded at this moment.');
+					dispatch({ type: LOAD_USER_DATA, userData: snapshot.val() });
+				}
+			});
+	};
 }
 
 export function setProfileImageURL(profileImg) {
-	return { type: SET_PROFILE_IMAGE_URL, profileImg: profileImg };
+	return function (dispatch, getStore) {
+		const emailHash = md5(getStore().main.user.email);
+		const GRAVATARENDPOINT = `https://www.gravatar.com/avatar/${emailHash}.jpg?s=200`;
+		dispatch({
+			type: SET_PROFILE_IMAGE_URL,
+			profileImg: profileImg ? profileImg : GRAVATARENDPOINT ? GRAVATARENDPOINT : profileImg,
+		});
+	};
+	// return { type: SET_PROFILE_IMAGE_URL, profileImg: profileImg };
 }
 
 /*
@@ -31,7 +81,11 @@ const MainReducer = (state = initialState, action) => {
 		case LOAD_USER_DATA:
 			return {
 				...state,
-				user: action.userData,
+				user: {
+					...state.user,
+					...action.userData,
+					profileImg: action.profileImg,
+				},
 			};
 		case SET_PROFILE_IMAGE_URL:
 			return {
